1e85b0d7b53801380b94a65014b9f3d8bd94585e
MuriloAbranches <muriloads@hotmail.com>
2024-11-26 22:57:45 -0300

    add logs

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-redis/redis/v8"
 )
 
 func main() {
+	log.Println("Starting Redis client...")
 	client := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
@@ -14,9 +16,13 @@ func main() {
 	persistence := NewRedisPersistence(client)
 	rateLimiter := NewRateLimiter(persistence)
 
+	log.Println("Setting up HTTP handler...")
 	http.Handle("/", rateLimiter.Middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})))
 
-	http.ListenAndServe(":8080", nil)
+	log.Println("Starting HTTP server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
 }
--- redis_persistence.go
+++ redis_persistence.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -16,21 +17,46 @@ func NewRedisPersistence(client *redis.Client) *RedisPersistence {
 }
 
 func (rp *RedisPersistence) Get(ctx context.Context, key string) (int, error) {
-	return rp.client.Get(ctx, key).Int()
+	log.Printf("Getting value for key: %s", key)
+	value, err := rp.client.Get(ctx, key).Int()
+	if err != nil {
+		log.Printf("Error getting value for key %s: %v", key, err)
+	}
+	return value, err
 }
 
 func (rp *RedisPersistence) Incr(ctx context.Context, key string) error {
-	return rp.client.Incr(ctx, key).Err()
+	log.Printf("Incrementing value for key: %s", key)
+	err := rp.client.Incr(ctx, key).Err()
+	if err != nil {
+		log.Printf("Error incrementing value for key %s: %v", key, err)
+	}
+	return err
 }
 
 func (rp *RedisPersistence) Expire(ctx context.Context, key string, expiration time.Duration) error {
-	return rp.client.Expire(ctx, key, expiration).Err()
+	log.Printf("Setting expiration for key: %s to %v", key, expiration)
+	err := rp.client.Expire(ctx, key, expiration).Err()
+	if err != nil {
+		log.Printf("Error setting expiration for key %s: %v", key, err)
+	}
+	return err
 }
 
 func (rp *RedisPersistence) Set(ctx context.Context, key string, value int, expiration time.Duration) error {
-	return rp.client.Set(ctx, key, value, expiration).Err()
+	log.Printf("Setting value for key: %s to %d with expiration %v", key, value, expiration)
+	err := rp.client.Set(ctx, key, value, expiration).Err()
+	if err != nil {
+		log.Printf("Error setting value for key %s: %v", key, err)
+	}
+	return err
 }
 
 func (rp *RedisPersistence) TTL(ctx context.Context, key string) (time.Duration, error) {
-	return rp.client.TTL(ctx, key).Result()
+	log.Printf("Getting TTL for key: %s", key)
+	ttl, err := rp.client.TTL(ctx, key).Result()
+	if err != nil {
+		log.Printf("Error getting TTL for key %s: %v", key, err)
+	}
+	return ttl, err
 }
